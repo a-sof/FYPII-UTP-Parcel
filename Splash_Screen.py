b52d4ddaecac447f5b8eeacca1bbcc1a43e1ec2e
chopiya <114756420+a-sof@users.noreply.github.com>
2024-10-25 10:47:53 +0800

    Update Splash_Screen.py

--- Splash_Screen.py
+++ Splash_Screen.py
@@ -61,5 +61,5 @@ with placeholder.container():
 placeholder.empty()
 
 # Switch to the selected page
-st.switch_page("./pages/FeatureOffering.py")  # Use the exact name without .py
+st.switch_page("./pages/FeatureOffering")  # Use the exact name without .py
 
